a10159b53cd99bc471e66850a95ef41b4f9bcb35
Ziplaw <afrgplus@gmail.com>
2021-11-09 11:30:48 +0100

    test

---
+++ Assets/Project/Scripts/Tool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Tool : MonoBehaviour, IUseObjectData, Interactor
+{
+    private ToolData _toolData;
+    public ObjectData Data => _toolData;
+    public void Interact(Interactable interactable)
+    {
+        throw new System.NotImplementedException();
+    }
+}
