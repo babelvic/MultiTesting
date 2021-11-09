9ef440ade1c2367b5c535badd2292f4e2d9ddb7d
Ziplaw <afrgplus@gmail.com>
2021-11-09 10:30:14 +0100

    test

--- Assets/Project/Scripts/TopDownMovement.cs
+++ Assets/Project/Scripts/TopDownMovement.cs
@@ -1,16 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Composites;
 using UnityEngine.InputSystem.HID;
 
-[RequireComponent(typeof(Rigidbody),typeof(BoxCollider))]
-public class TopDownMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class TopDownMovement : MonoBehaviour, IPunObservable
 {
     public float speed = 5;
     public Vector3 movement;
+    private PhotonView _photonView;
     private Rigidbody rb;
     public Action<InputAction.CallbackContext> OnTopButtonPress;
     public Action<InputAction.CallbackContext> OnBottomButtonPress;
@@ -20,41 +22,57 @@ public class TopDownMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _photonView = GetComponent<PhotonView>();
     }
 
     void Update()
     {
-        if (movement.sqrMagnitude > 0) transform.rotation = Quaternion.LookRotation(movement, Vector3.up);
+        if (_photonView.IsMine)
+            if (movement.sqrMagnitude > 0)
+                transform.rotation = Quaternion.LookRotation(movement, Vector3.up);
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = transform.forward * movement.sqrMagnitude * speed;
+        if (_photonView.IsMine)
+            rb.velocity = transform.forward * movement.sqrMagnitude * speed;
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        var input = ctx.ReadValue<Vector2>();
-        movement = new Vector3(input.x, 0, input.y);
+        if (_photonView.IsMine)
+        {
+            var input = ctx.ReadValue<Vector2>();
+            movement = new Vector3(input.x, 0, input.y);
+        }
     }
 
     public void OnTop(InputAction.CallbackContext ctx)
     {
-        OnTopButtonPress?.Invoke(ctx);
+        if (_photonView.IsMine)
+            OnTopButtonPress?.Invoke(ctx);
     }
 
     public void OnBottom(InputAction.CallbackContext ctx)
     {
-        OnBottomButtonPress?.Invoke(ctx);
+        if (_photonView.IsMine)
+            OnBottomButtonPress?.Invoke(ctx);
     }
 
     public void OnLeft(InputAction.CallbackContext ctx)
     {
-        OnRightButtonPress?.Invoke(ctx);
+        if (_photonView.IsMine)
+            OnRightButtonPress?.Invoke(ctx);
     }
 
     public void OnRight(InputAction.CallbackContext ctx)
     {
-        OnLeftButtonPress?.Invoke(ctx);
+        if (_photonView.IsMine)
+            OnLeftButtonPress?.Invoke(ctx);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        //serialization
     }
 }
